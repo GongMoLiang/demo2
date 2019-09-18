271855c9a04af50e048d812d194dfdddfe00716c
GongMoLiang <gml18720826651@163.comm>
2019-09-18 11:11:57 +0800

    修改

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const exprsse=require("express");
 const server=exprsse();
+server.use(exprsse.json());
+server.use(exprsse.urlencoded({extended:true}));
 server.get("/",(req,res)=>{
     res.send("你好 expres框架")
 });
+server.get("/hello",(req,res)=>{
+    console.log(req.query);
+    res.send("你好 二狗")
+});
+server.post("/handle",(req,res)=>{
+    console.log(req.body);
+    res.send("中国")
+})
 server.listen(3000);
