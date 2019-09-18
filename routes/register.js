e880c75e734dca2e582bd0b4523c4e4cafccb4b6
GongMoLiang <gml18720826651@163.comm>
2019-09-18 17:20:57 +0800

    注册页面完成

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,8 @@
 const express=require("express");
 const router=express.Router();
+router.get("/",(req,res)=>{
+    res.render("register.ejs")
+});
 router.post("/handle",(req,res)=>{
     console.log(req.body);
     res.send("注册处理")
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const regrouter=require("./routes/register");
 const server=express();
 const cookieParser=require("cookie-parser");
 
+//引入模板引擎ejs
+server.set("views","views");
+server.set("views engine","ejs");
 
 //中间件引入req.body属性
 server.use(express.json());
@@ -12,9 +15,7 @@ server.use(express.urlencoded({extended:true}));
 //设置静态托管文件夹
 server.use(express.static("www"));
 
-//引入模板引擎ejs
-server.set("views","views");
-server.set("views egine","ejs");
 
-server.use("/regsiter",regrouter);
+
+server.use("/register",regrouter);
 server.listen(3000);
